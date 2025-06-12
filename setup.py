7298db4a0afcc5ad24df039f45604ee2aba8b3e9
Bryan Luis Pineda <155897429+Bryanluispg@users.noreply.github.com>
2025-06-12 11:58:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
+################################################
+#                   BLPG                       #   
+################################################
+
 from setuptools import setup, find_packages
 
 setup(
-    name="sap_service_layer",
-    version="0.1.0",
+    name="SAPB1SL",
+    version="1.3.0",
     author="Bryan Pineda Gonzalez",
     author_email="dev@bryanlpinedag.com",
-    description="Cliente Python para el Service Layer de SAP Business One usando requests",
+    description="Cliente Python para el Service Layer de SAP Business One usando requests.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Bryanluispg/sap_service_layer_blpg",  
+    url="https://github.com/Bryanluispg/SAPB1SL",  
     packages=find_packages(),
     install_requires=[
-        "requests"
+        "requests",
+        "urllib3"
     ],
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
